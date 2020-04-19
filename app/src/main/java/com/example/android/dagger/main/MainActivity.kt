c12454918323d98fdaca0dcd14565006b069e8c8
Federico Creti <federico.creti87@gmail.com>
2020-04-19 16:48:17 +0200

    Added RegistrationComponent sub component with ActivityScope

--- app/src/main/java/com/example/android/dagger/main/MainActivity.kt
+++ app/src/main/java/com/example/android/dagger/main/MainActivity.kt
@@ -26,8 +26,13 @@ import com.example.android.dagger.R
 import com.example.android.dagger.login.LoginActivity
 import com.example.android.dagger.registration.RegistrationActivity
 import com.example.android.dagger.settings.SettingsActivity
+import com.example.android.dagger.user.UserManager
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var userManager: UserManager
 
     private lateinit var mainViewModel: MainViewModel
 
@@ -37,9 +42,11 @@ class MainActivity : AppCompatActivity() {
      * else carry on with MainActivity
      */
     override fun onCreate(savedInstanceState: Bundle?) {
+        (application as MyApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
 
-        val userManager = (application as MyApplication).userManager
+        //val userManager = (application as MyApplication).userManager
+
         if (!userManager.isUserLoggedIn()) {
             if (!userManager.isUserRegistered()) {
                 startActivity(Intent(this, RegistrationActivity::class.java))
